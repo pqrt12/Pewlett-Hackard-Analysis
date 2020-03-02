5931538bd166896c0f3a5dbff58d3ea873a0300c
pqrt12 <54596601+pqrt12@users.noreply.github.com>
2020-03-01 16:58:19 -0800

    uniq name retire employees

--- Queries/challenge.sql
+++ Queries/challenge.sql
@@ -2,7 +2,7 @@
 /* 
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-	dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     dept_name VARCHAR(40) NOT NULL,
     PRIMARY KEY (dept_no),
     UNIQUE (dept_name)
@@ -17,126 +17,127 @@ CREATE TABLE employees (
     PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE salaries (
-  	emp_no INT NOT NULL,
-  	salary INT NOT NULL,
-  	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
-  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-  	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
-  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),	
-  	PRIMARY KEY (emp_no)
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no)
 );
 CREATE TABLE titles (
-  	emp_no INT NOT NULL,
-  	title varchar NOT NULL,
-  	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
-  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+    emp_no INT NOT NULL,
+    title varchar NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no)
 );
 */
 
 -- Challenge
+-----------------------------------------------------------------
+-- create retire_emp by inner_join tables "employees", "titles"
+-- and "salaries", see ERD.
+--      1, current employees
+--      2, born between [1952, 1955].
+--      3, hired between [1985, 1988].
 
--- Challenge Part 1
-
--- ensure all employees holds a title, have a job (a slot in corporate org chart).
--- current employees have a title
-SELECT COUNT(emp_no)
-FROM dept_emp
-where (to_date = '9999-01-01');
--- results: 240124
-
--- current employees have a job
-SELECT COUNT(emp_no)
-FROM titles
-where (to_date = '9999-01-01');
--- results: 240124
-
--- current employees have a title, and have a job.
--- inner
-SELECT count(de.emp_no)
-FROM (SELECT * FROM dept_emp
-		where (to_date = '9999-01-01')) as de
-	inner join (SELECT * FROM titles
-		where (to_date = '9999-01-01')) as t
-	on (de.emp_no = t.emp_no);
--- 240124
-
--- current employees have either a title, or a job.
--- full outer
-SELECT count(de.emp_no)
-FROM (SELECT * FROM dept_emp
-		where (to_date = '9999-01-01')) as de
-	full outer join (SELECT * FROM titles
-		where (to_date = '9999-01-01')) as t
-	on (de.emp_no = t.emp_no);
--- 240124
-
--- create retiring_emp by inner_join tables employee and titles.
--- 1, current employees
--- 2, born in [1952, 1955].
--- 3, hired in [1985, 1988].
+DROP TABLE IF EXISTS retire_emp;
 SELECT e.emp_no,
-	e.first_name,
-	e.last_name,
-	t.title,
-	t.from_date,
-	s.salary
-INTO retiring_emp
+    e.first_name,
+    e.last_name,
+    t.title,
+    t.from_date,
+    s.salary
+INTO retire_emp
 FROM employees as e
-	INNER JOIN salaries as s
-		ON (e.emp_no = s.emp_no)
-	INNER JOIN titles as t
-		ON (e.emp_no = t.emp_no)
+    INNER JOIN salaries as s
+        ON (e.emp_no = s.emp_no)
+    INNER JOIN titles as t
+        ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
-    AND (t.to_date = '9999-01-01')
-ORDER BY e.emp_no;
-
--- how many are they?
-SELECT COUNT(emp_no) FROM retiring_emp;
+    AND (t.to_date = '9999-01-01');
 -- 33118
 
--- show how to get all employees, retirees with last titles, current employees with latest tiles.
--- emp_no, title, latest_date
-SELECT emp_no, title, max(to_date) as latest_date
-FROM titles
-GROUP BY emp_no, title
-ORDER BY emp_no;
+-- how many are they?
+SELECT COUNT(emp_no) FROM retire_emp;
+-- 33118
 
--- mentors, inner join tables employees and titles
+-----------------------------------------------------------------
+-- many employees "emp_no" share same names, try to
+-- remove duplicated entries.
+--
+-- remove duplicated entries from same person:
+--      with same name ("first_name", "last_name")
+--      with same "birth_date"
+--
+-- inner_join tables "employees" and "retire_map" to get associated
+--      "birth_date" and "hire_date";
+-- keep later "hire_date" entry for same person (with same name and
+--      "birth_date").
+DROP TABLE IF EXISTS retire_emp_unique;
+SELECT DISTINCT ON (first_name, last_name, birth_date)
+    emp_no, first_name, last_name, title, from_date, salary
+INTO retire_emp_unique
+FROM (SELECT e.*, re.title, re.from_date, re.salary
+    FROM employees as e
+    INNER JOIN retire_emp as re
+        ON (e.emp_no = re.emp_no)
+    ORDER BY e.hire_date DESC) as dup;
+-- 33118
+
+-- how many are they?
+SELECT COUNT(emp_no) FROM retire_emp_unique;
+-- 33118
+
+--  remove duplicate entries with same name in table "retire_emp".
+--      keep lastest "from_date" entries.
+SELECT DISTINCT ON (first_name, last_name) *
+INTO retire_emp_uniq
+FROM (SELECT *
+      FROM retire_emp
+      ORDER BY from_date DESC) as dup;
+-- 32859
+
+-- how many are they?
+SELECT COUNT(emp_no) FROM retire_emp_uniq;
+-- 32859
+
+-----------------------------------------------------------------
+-- mentors, inner join tables "employees" and "titles"
 -- 1, current employees
 -- 2, born in 1965.
-SELECT e.emp_no,
-	e.first_name,
-	e.last_name,
-	t.title,
-	t.from_date,
-	t.to_date
+-- duplicate entries with same name is removed.
+DROP TABLE IF EXISTS mentors;
+SELECT DISTINCT ON (first_name, last_name) *
 INTO mentors
-FROM employees as e
-	INNER JOIN titles as t
-		ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-    AND (t.to_date = '9999-01-01')
-ORDER BY e.emp_no;
+FROM (SELECT e.emp_no, e.first_name, e.last_name,
+        t.title, t.from_date, t.to_date
+    FROM employees as e
+        INNER JOIN titles as t
+            ON (e.emp_no = t.emp_no)
+    WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+        AND (t.to_date = '9999-01-01')
+    ORDER BY from_date DESC) as mentors_dup;
 
 -- how many are mentors?
 SELECT COUNT(emp_no) FROM mentors;
